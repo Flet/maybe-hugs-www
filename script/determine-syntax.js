15a82f20739ab6018e157bbe178d9f58dccab835
Dan Flettre <fletd01@yahoo.com>
2015-09-10 16:09:34 -0500

    add determine-syntax.js

---
+++ script/determine-syntax.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+var langs = {
+  'clj': 'clojure',
+  'hs': 'haskell',
+  'pm': 'perl',
+  't': 'perl',
+  'PL': 'perl',
+  'rb': 'ruby',
+  'rs': 'rust',
+  'py': 'python'
+};
+
+module.exports = function (filePath) {
+  var ext = path.extname(filePath).slice(1);
+  return langs[ext] || ext;
+};
